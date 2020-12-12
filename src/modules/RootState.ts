95ffd2ec20f6f4d8566881b6efe9c522f2933be0
Eirik Måseidvåg <eirik.maaseidvaag@gmail.com>
2020-12-12 12:57:31 -0800

    New rootstate structure

--- src/modules/RootState.ts
+++ src/modules/RootState.ts
@@ -2,11 +2,22 @@ import { RoomState } from './room';
 import { StageState } from './stage';
 import { Collection, Tactic } from './tactic';
 import { User } from './user';
+import { CanvasElement, CanvasElementHistory } from './canvas';
+import { Team } from './team';
 
-export interface RootState {
+export interface State {
     collections: { [key: string]: Collection };
     users: { [key: string]: User };
     tactics: { [key: string]: Tactic };
     room: RoomState;
-    stage: { [key: string]: StageState };
+}
+
+export interface RootState extends State, StateItems {
+}
+
+export interface StateItems {
+    stages: { [key: string]: StageState };
+    canvasElements: { [key: string]: CanvasElement[] };
+    canvasElementHistories: { [key: string]: CanvasElementHistory[] };
+    teams: { [key: string]: Team[] };
 }
