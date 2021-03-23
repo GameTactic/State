087b072f8e98482d6e7d966d0600fb8c4962c0ab
Eirik Måseidvåg <eirik.maaseidvaag@gmail.com>
2021-03-23 18:07:41 +0100

    Fix disconnection out doc

--- src/socket/docs/lifecycle/disconnection/DisconnectionOutDoc.ts
+++ src/socket/docs/lifecycle/disconnection/DisconnectionOutDoc.ts
@@ -1,7 +1,7 @@
 import SocketDoc, { SocketEventType } from '../../types';
 import LifecycleHistoryDisconnection from '../../../../history/lifecycle/disconnection/LifecycleHistoryDisconnection';
 
-export default class DisconnectionInDoc implements SocketDoc {
+export default class DisconnectionOutDoc implements SocketDoc {
     title = 'Disconnect';
     description = 'Disconnects user from socket';
     type = SocketEventType.IN_AND_OUT;
