d7018dff9a0b04ff685daa537c025bfcedd964e6
Eirik Måseidvåg <eirik.maaseidvaag@gmail.com>
2021-03-30 10:32:39 +0200

    Add more to partial state

--- src/socket/state/types.ts
+++ src/socket/state/types.ts
@@ -10,23 +10,26 @@ import History from '../../history/History';
 export interface SocketStateOptions {
     collection: RootCollection;
     users: { [jti: string]: User };
-    tactics: { [id: string]: TacticData }
-    history: { [id: string]: History }
-    room: Room
-    [key: string]: any
+    tactics: { [id: string]: TacticData };
+    history: { [id: string]: History };
+    room: Room;
+    [key: string]: any;
 }
 
 export interface TacticData {
     tactic: Tactic;
-    teams: { [id: string]: Team }
-    elements: { [id: string]: CanvasElement }
+    teams: { [id: string]: Team };
+    elements: { [id: string]: CanvasElement };
     map: Map;
 }
 
 export interface PartialState {
-    room: Room,
-    collection: RootCollection,
-    users: { [jti: string]: User },
-    history: { [id: string]: History },
-    tactics: { [id: string]: Tactic }
+    room: Room;
+    collection: RootCollection;
+    users: { [jti: string]: User };
+    history: { [id: string]: History };
+    tactics: { [id: string]: Tactic };
+    map?: Map;
+    elements?: { [id: string]: CanvasElement };
+    teams?: { [id: string]: Team };
 }
