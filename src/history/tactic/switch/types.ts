bb83cff2f271d59a89e89a5f8f06672f36b5df7f
Eirik Måseidvåg <eirik.maaseidvaag@gmail.com>
2021-05-21 23:56:02 +0200

    .

--- src/history/tactic/switch/types.ts
+++ src/history/tactic/switch/types.ts
@@ -1,7 +1,7 @@
 import { AbstractHistoryOptions } from '../../types';
 
 export interface TacticHistorySwitchOptions extends AbstractHistoryOptions {
-    oldTacticId: string | undefined;
-    newTacticId: string | undefined;
     userId: string;
+    newTacticId: string | undefined;
+    oldTacticId: string | undefined;
 }
--- src/history/team/switch/types.ts
+++ src/history/team/switch/types.ts
@@ -2,6 +2,6 @@ import { AbstractHistoryOptions } from '../../types';
 
 export interface TeamHistorySwitchOptions extends AbstractHistoryOptions {
     userId: string;
-    newTeamId: string;
-    oldTeamId: string;
+    newTeamId: string | undefined;
+    oldTeamId: string | undefined;
 }
