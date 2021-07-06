a9e16c9893612072afaebb979a35a8726ffabb5f
Eirik Måseidvåg <eirik.maaseidvaag@gmail.com>
2021-07-06 16:30:01 +0200

    Add rotation to TransformationChange

--- src/history/canvas/transformation/types.ts
+++ src/history/canvas/transformation/types.ts
@@ -5,14 +5,20 @@ export interface CanvasHistoryTransformationOptions extends AbstractHistoryOptio
   changes: Array<TransformationChange>;
 }
 
+/**
+ * Dimensions are not included as it is not possible to change the initial width and height
+ * Instead the scale should be changed
+ */
 export interface TransformationChange {
   elementId: string;
   from: {
-    local: Matrix;
-    parent: Matrix;
+    rotation: number;
+    transform: Matrix;
+    parentTransform: Matrix;
   };
   to: {
-    local: Matrix;
-    parent: Matrix;
+    rotation: number;
+    transform: Matrix;
+    parentTransform: Matrix;
   };
 }
