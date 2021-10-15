d57fe7c399061d97c8f77affb9c7653b07d61159
Eirik Måseidvåg <eirik.maaseidvaag@gmail.com>
2021-10-15 16:52:11 +0200

    Add WOT, WOWS and CSGO to games

--- src/canvas/map/types.ts
+++ src/canvas/map/types.ts
@@ -1,9 +1,11 @@
 import View from './view/View';
+import { Game } from '../../games';
 
 export interface MapOptions {
     id?: string;
     name: string;
     description: string;
+    game: Game;
     views: Array<View>;
     cover: string;
     [key: string]: any;
--- src/games/types.ts
+++ src/games/types.ts
@@ -1,3 +1,6 @@
 export enum Game {
+    WOT = 'wot',
+    WOWS = 'wows',
+    CSGO = 'csgo',
     R6 = 'r6'
 }
