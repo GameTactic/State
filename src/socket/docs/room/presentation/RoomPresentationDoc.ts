9609c0359514cf035a1cdedf82579156fec6d557
Eirik Måseidvåg <eirik.maaseidvaag@gmail.com>
2021-12-08 06:44:51 +0100

    Fix type name

--- src/socket/docs/room/presentation/RoomPresentationDoc.ts
+++ src/socket/docs/room/presentation/RoomPresentationDoc.ts
@@ -15,6 +15,6 @@ export default class RoomPresentationDoc extends RoomSocketDoc {
         });
     }
 }
-export type RoomDisconnectDocPayload = {
+export type RoomPresentationDocPayload = {
     history: RoomHistoryPresentation;
 };
