7a2c19dbd8248831182b21d6e91507e91c87b4d8
Eirik Måseidvåg <eirik.maaseidvaag@gmail.com>
2021-12-11 04:07:30 +0100

    Export pencil

--- src/canvas/elements/index.ts
+++ src/canvas/elements/index.ts
@@ -2,5 +2,6 @@ export * from './abstract';
 export * from './circle';
 export * from './entity';
 export * from './icon';
+export * from './pencil';
 export * from './ping';
 export * from './plugin';
